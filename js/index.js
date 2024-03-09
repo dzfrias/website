a7f631211cf26cad672a96b20caa0e3985778750
dzfrias <dzfrias@users.noreply.github.com>
2024-03-09 15:20:59 +0000

    deploy: 951d3acaf1efdfa0799687e9764e651c53f433cc

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
-document.querySelectorAll("img").forEach((img) => {
-  img.addEventListener("click", (evt) => {
-    const imgClone = evt.target.cloneNode(true);
-    const dialog = document.createElement("dialog");
-    dialog.appendChild(imgClone);
-    document.body.appendChild(dialog);
-    dialog.showModal();
-    dialog.addEventListener("click", (e) => {
-      const rect = e.target.getBoundingClientRect();
-      const clickedInDialog =
-        rect.top <= e.clientY &&
-        e.clientY <= rect.top + rect.height &&
-        rect.left <= e.clientX &&
-        e.clientX <= rect.left + rect.width;
+document.querySelectorAll(".expand-img").forEach((div) => {
+  const btn = div.children[0];
+  const dialog = div.children[1];
+  btn.addEventListener("click", () => dialog.showModal());
+  dialog.addEventListener("click", (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const clickedInDialog =
+      rect.top <= e.clientY &&
+      e.clientY <= rect.top + rect.height &&
+      rect.left <= e.clientX &&
+      e.clientX <= rect.left + rect.width;
 
-      if (!clickedInDialog) {
-        document.body.removeChild(e.target);
-      }
-    });
+    if (!clickedInDialog) {
+      dialog.close();
+    }
   });
 });
