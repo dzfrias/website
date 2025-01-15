ac9133ca6703b0f9b9da46ba906170012c837125
Diego Frias <styx5242@gmail.com>
2025-01-14 20:24:27 -0800

    Update eleventy config
    Use ESM modules, fix eleventy version to 3.0.0 for stability, and rename
    `.eleventy.js` to `eleventy.config.js`.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,16 @@
-const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const md = require("markdown-it")({ html: true });
-const markdownItAnchor = require("markdown-it-anchor");
-const eleventyGoogleFonts = require("eleventy-google-fonts");
-const pluginTOC = require("eleventy-plugin-toc");
-const { minify } = require("terser");
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import markdownItKatex from "markdown-it-katex";
+import markdownItFootnote from "markdown-it-footnote";
+import eleventyGoogleFonts from "eleventy-google-fonts";
+import pluginTOC from "eleventy-plugin-toc";
+import { minify } from "terser";
 
-module.exports = (config) => {
+const md = markdownIt({ html: true });
+
+export default function (eleventyConfig) {
   const proxy = (tokens, idx, options, _env, self) =>
     self.renderToken(tokens, idx, options);
 
@@ -16,6 +20,7 @@ module.exports = (config) => {
     return `<div class="expand-img"><button aria-haspopup="dialog" aria-label="Expand image">${defaultImageRender(tokens, idx, options, env, self)}</button><dialog>${defaultImageRender(tokens, idx, options, env, self)}</dialog></div>`;
   };
 
+  eleventyConfig.setLibrary("md", md.use(markdownItFootnote));
   // Override the way headers are rendered in markdown to be shifted by 1
   // Taken from https://github.com/markdown-it/markdown-it/issues/871#issuecomment-1752196424
   const defaultHeadingOpenRenderer = md.renderer.rules["heading_open"] || proxy;
@@ -55,7 +60,6 @@ module.exports = (config) => {
     return "</aside>";
   };
 
-  config.setLibrary("md", md.use(require("markdown-it-footnote")));
   // Default footnote renderer without brackets
   md.renderer.rules.footnote_caption = function (tokens, idx) {
     let n = Number(tokens[idx].meta.id + 1).toString();
@@ -88,34 +92,34 @@ module.exports = (config) => {
     '<ol class="footnotes-list">\n';
   md.renderer.rules.footnote_block_close = () => "</ol>" + "</section>";
 
-  config.addPassthroughCopy("./src/img/");
-  config.addPassthroughCopy("./src/fonts/");
+  eleventyConfig.addPassthroughCopy("./src/img/");
+  eleventyConfig.addPassthroughCopy("./src/fonts/");
   // Favicons
-  config.addPassthroughCopy({ "./src/favicon/*": "." });
+  eleventyConfig.addPassthroughCopy({ "./src/favicon/*": "." });
 
-  config.setLibrary("md", md.use(require("markdown-it-katex")));
-  config.setLibrary(
+  eleventyConfig.setLibrary("md", md.use(markdownItKatex));
+  eleventyConfig.setLibrary(
     "md",
     md.use(markdownItAnchor, {
       permalink: markdownItAnchor.permalink.headerLink(),
     }),
   );
 
-  config.addCollection("sortedPosts", function (collectionApi) {
+  eleventyConfig.addCollection("sortedPosts", function (collectionApi) {
     return collectionApi.getFilteredByTag("posts").sort(function (a, b) {
       return b.date - a.date;
     });
   });
 
-  config.addPlugin(eleventyGoogleFonts);
-  config.addPlugin(syntaxHighlight, {
+  eleventyConfig.addPlugin(eleventyGoogleFonts);
+  eleventyConfig.addPlugin(syntaxHighlight, {
     preAttributes: {
       class: ({ language }) => `language-${language || "plain"}`,
     },
     errorOnInvalidLanguage: false,
   });
-  config.addPlugin(pluginTOC);
-  config.addPlugin(eleventyImageTransformPlugin, {
+  eleventyConfig.addPlugin(pluginTOC);
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     extensions: "html",
     formats: ["webp", "jpeg"],
     defaultAttributes: {
@@ -124,7 +128,7 @@ module.exports = (config) => {
     },
   });
 
-  config.addFilter("jsmin", async function (code) {
+  eleventyConfig.addFilter("jsmin", async function (code) {
     try {
       const minified = await minify(code, { module: true });
       return minified.code;
@@ -135,7 +139,7 @@ module.exports = (config) => {
   });
 
   // Watch CSS files for changes
-  config.setBrowserSyncConfig({
+  eleventyConfig.setBrowserSyncConfig({
     files: ["dist/css/*.css"],
   });
 
@@ -148,4 +152,4 @@ module.exports = (config) => {
       output: "dist",
     },
   };
-};
+}
